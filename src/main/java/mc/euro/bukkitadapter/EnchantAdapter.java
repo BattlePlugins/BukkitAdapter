68196a3f354f6586b8c674a4c4f18c2dd03ac713
RednedEpic <redned@retronixgames.x10host.com>
2019-05-18 15:41:50 -0500

    Refactor EnchantAdapter - Hopefully this can be used to help clean up methods in InventoryUtil

--- src/main/java/mc/euro/bukkitadapter/EnchantAdapter.java
+++ src/main/java/mc/euro/bukkitadapter/EnchantAdapter.java
@@ -1,6 +1,7 @@
 package mc.euro.bukkitadapter;
 
 
+import mc.euro.bukkitadapter.material.BattleEnchant;
 import org.bukkit.enchantments.Enchantment;
 
 import java.util.Collections;
@@ -16,10 +17,7 @@ import java.util.Map;
  *
  */
 
-// TODO: Refactor this class to be similar to MaterialAdapter
 public class EnchantAdapter {
-
-    private static final Map<Integer, String> emap = initMap();
 
     /**
      * This method can return null.
@@ -27,69 +25,10 @@ public class EnchantAdapter {
      *
      * @param ench - Enchant name or ID
      * @return org.bukkit.enchantments.Enchantment found on the running server or null.
+     * @deprecated use parseEnchant()
      */
+    @Deprecated
     public static Enchantment getEnchantment(String ench) {
-        Enchantment enchantment = Enchantment.getByName(ench);
-        if (enchantment == null) {
-            int value = Integer.MIN_VALUE;
-
-            try {
-                value = Integer.valueOf(ench);
-            } catch (NumberFormatException ignored) {/* do nothing*/}
-
-            enchantment = Enchantment.getByName(emap.get(value));
-        }
-
-        return enchantment;
-    }
-
-    private static Map<Integer, String> initMap() {
-        Map<Integer, String> temp = new HashMap<>();
-
-        // Key = id
-        // Value = name
-        temp.put(0, "PROTECTION_ENVIRONMENTAL");
-        temp.put(1, "PROTECTION_FIRE");
-        temp.put(2, "PROTECTION_FALL");
-        temp.put(3, "PROTECTION_EXPLOSIONS");
-        temp.put(4, "PROTECTION_PROJECTILE");
-        temp.put(5, "OXYGEN");
-        temp.put(6, "WATER_WORKER");
-        temp.put(7, "THORNS");
-        temp.put(8, "DEPTH_STRIDER");
-        temp.put(9, "FROST_WALKER");
-        temp.put(10, "BINDING_CURSE");
-
-        temp.put(16, "DAMAGE_ALL");
-        temp.put(17, "DAMAGE_UNDEAD");
-        temp.put(18, "DAMAGE_ARTHROPODS");
-        temp.put(19, "KNOCKBACK");
-        temp.put(20, "FIRE_ASPECT");
-        temp.put(21, "LOOT_BONUS_MOBS");
-        temp.put(22, "SWEEPING_EDGE");
-
-        temp.put(32, "DIG_SPEED");
-        temp.put(33, "SILK_TOUCH");
-        temp.put(34, "DURABILITY");
-        temp.put(35, "LOOT_BONUS_BLOCKS");
-
-        temp.put(48, "ARROW_DAMAGE");
-        temp.put(49, "ARROW_KNOCKBACK");
-        temp.put(50, "ARROW_FIRE");
-        temp.put(51, "ARROW_INFINITE");
-
-        temp.put(61, "LUCK");
-        temp.put(62, "LURE");
-
-        temp.put(70, "MENDING");
-        temp.put(71, "VANISHING_CURSE");
-
-        // For enchantments that do not have an ID (>1.13)
-        temp.put(-1, "CHANNELING");
-        temp.put(-2, "IMPALING");
-        temp.put(-3, "LOYALTY");
-        temp.put(-4, "RIPTIDE");
-
-        return Collections.unmodifiableMap(temp);
+       return BattleEnchant.fromString(ench).parseEnchant();
     }
 }
